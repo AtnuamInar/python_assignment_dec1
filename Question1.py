666f9a2f616af9e4c74b87a482f10a603c0c5cd6
AtnuamInar <mauntarani88@gmail.com>
2017-12-03 16:01:16 +0545

    All Script until now

---
+++ Question1.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+
+fname=input('Enter Customers Name')
+address=input('enter your Address')
+items=input('Enter the number of items brought')
+
+price=float(items)*100
+
+print('_____________________________________________________')
+print('Welcome to Fuchu Supermarket')
+print('_____________________________________________________')
+
+
+print("HI {0}, here is your bill:".format(fname))
+print('You brought {0} items'.format(items))
+print('Total price={}'.format(price))
+
+
